f8c924af41a21f600bab4e26870f6c4831da88fd
unknown <sajjan.kumar@quodeck.com>
2022-08-17 10:36:00 +0530

    first commit

---
+++ src/component/Contact.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div>
+      <div className="my-5">
+        <h1 className="text-center">Contact Us</h1>
+      </div>
+      <div className="container contact_div">
+        <div className="row">
+          <div className="col-md-6 col-10 mx-auto">
+            <form>
+              <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">
+                  Full Name
+                </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="exampleFormControlInput1"
+                  placeholder="Enter Full Name"
+                />
+              </div>
+              <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">
+                  Mobile Number
+                </label>
+                <input
+                  type="number"
+                  class="form-control"
+                  id="exampleFormControlInput1"
+                  placeholder="Enter Mobile Number"
+                />
+              </div>
+              <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="exampleFormControlInput1"
+                  placeholder="name@example.com"
+                />
+              </div>
+              <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">
+                  Message
+                </label>
+                <textarea
+                  class="form-control"
+                  id="exampleFormControlTextarea1"
+                  rows="3"
+                ></textarea>
+              </div>
+              <div class="col-12">
+                <button class="btn btn-outline-primary" type="submit">
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
