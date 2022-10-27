3473c937af0313d09ca2b9e7981d6af4407394c7
unknown <sajjan.kumar@quodeck.com>
2022-10-27 19:20:53 +0530

    Deploye

--- src/component/Card.js
+++ src/component/Card.js
@@ -5,7 +5,12 @@ function Card(props) {
   return (
     <div className="col-md-4 col-10 mx-auto">
       <div className="card">
-        <img src={props.imgsrc} className="card-img-top" alt="..." />
+        <img
+          src={props.imgsrc}
+          className="card-img-top"
+          alt="..."
+          style={{ height: 200 }}
+        />
         <div className="card-body">
           <h5 className="card-title font-weight-bold">{props.title}</h5>
           <p className="card-text">
